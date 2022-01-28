0b0df802531bd229a59a898d4da6b361beb97883
Agamdeep Singh <agamdeep579@gmail.com>
2022-01-28 21:37:00 +0530

    Updated the code

--- main.py
+++ main.py
@@ -8,13 +8,6 @@
 # Define the alphabetss
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# Get the inputs from the user
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-
-text = input("Type your message:\n").lower()
-
-shift = int(input("Type the shift number:\n"))
-
 def caesar(text, shift, direction):
     final_text = ""
 
@@ -43,4 +36,27 @@ def caesar(text, shift, direction):
     else:
         print("Sorry, this function is not available. Maybe you made an Typo!")
 
-caesar(text = text, shift = shift, direction = direction)
+# Print the logo from art.py
+from art import logo
+print(logo)
+
+# To loop again
+should_end = False
+while not should_end:
+    # Get the inputs from the user
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+
+    text = input("Type your message:\n").lower()
+
+    shift = int(input("Type the shift number:\n"))
+
+    #If the user enters a shift that is greater than the number of letters in the alphabet
+    shift = shift % 26
+
+    caesar(text = text, shift = shift, direction = direction)
+
+    restart = input("Type 'yes' or 'no' to run again or not:\n")
+    
+    if restart == "no":
+        should_end = True
+        print("Thank you for using Caesar Cipher!")
